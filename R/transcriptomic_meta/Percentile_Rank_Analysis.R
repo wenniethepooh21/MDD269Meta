8fff81bab36c0c1462fd86e6e24be068c0a3d6f3
Wennie <cutewennie@hotmail.com>
2020-02-01 13:04:01 -0500

    created script to calculate the perctile rank of each gene

---
+++ R/transcriptomic_meta/Percentile_Rank_Analysis.R
@@ -0,0 +1,22 @@
+#Function that calculates the proportion of genes within the genome that has a smaller meta p_value than the current gene 
+#Which gene had the smallest meta p-value compared to the entire genome
+getRank <- function(dataset, num_genes){
+  dataset %<>% mutate(meta_Up = NA, meta_Down = NA)
+  Ramaker_Up <- dataset %>% filter(meta_direction == +1)
+  Ramaker_Down <- dataset %>% filter(meta_direction == -1) 
+  for (i in 1:length(dataset$gene_symbol)) {
+    gene_meta <- dataset$meta_p[i]
+    direction_val <- dataset$meta_direction[i]
+    if(direction_val == +1) {
+      dataset$meta_Up[i] <- Ramaker_Up %>% filter(meta_p <= gene_meta) %>% dplyr::select(gene_symbol) %>% nrow() / num_genes
+      dataset$meta_Down[i] <- (Ramaker_Down %>% filter(meta_p <= gene_meta) %>% dplyr::select(gene_symbol) %>% nrow() + 1) / (num_genes + 1)
+      
+      #this current gene will be in the down regulation list, offset it
+    }else if (direction_val == -1) {
+      dataset$meta_Up[i] <- (Ramaker_Up %>% filter(meta_p <= gene_meta) %>% dplyr::select(gene_symbol) %>% nrow() + 1) / (num_genes + 1)
+      dataset$meta_Down[i] <- Ramaker_Down %>% filter(meta_p <= gene_meta) %>% dplyr::select(gene_symbol) %>% nrow() / num_genes
+    }
+  }
+  dataset %<>% rowwise() %>% mutate(genome_percentile_rank = if_else(2 * min(meta_Up, meta_Down) > 1, 1, 2 * min(meta_Up, meta_Down)))
+  return(dataset)
+}
