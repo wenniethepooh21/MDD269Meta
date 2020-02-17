da211c871dfa76ec201bfab1935bbebe28dbc018
Wennie <cutewennie@hotmail.com>
2020-02-16 23:14:00 -0500

    updated with hypergeometic calculations

--- R/dataset_parsers/ZeiselEtAl.R
+++ R/dataset_parsers/ZeiselEtAl.R
@@ -1,6 +1,7 @@
 library(loomR)
 library(dplyr)
 library(homologene)
+library(here)
 
 #-------- read cell-type matrix data 
 loom_file <- connect(filename = here("Raw_Data/ZeiselEtAl/l6_r4.agg.loom"))
@@ -14,7 +15,7 @@ source(here("R/transcriptomic_meta/Max_Expression_Matrix.R"))
 max_cell_types <- get_max_expression(full_cell_type,"cell_type_taxon","mouse_gene")
 #for those genes that had a max expression level of 0, updated the cell_type description
 max_cell_types %<>% mutate(cell_type_taxon = if_else(expression_levels == 0,"Gene detected; No expression measured", cell_type_taxon))
-# max_cell_types %>% write_csv(here("Processed_Data/ZeiselEtAl/max_cell_type_expression.csv"))
+
 
 #---------
 #max cell type with PNS neurons taxon removed 
@@ -28,11 +29,9 @@ cns_cell_types <- cell_types %>% select(mouse_gene, cns_cell_type_taxa)
 #deal with duplicate genes - avg the expression
 cns_cell_types %<>% group_by(mouse_gene) %>% mutate_each(list(mean))  %>% distinct() %>% ungroup()
 #re-run max cell type 
-cns_max_cell_types <- get_max_expression(cns_cell_types,"cell_type_taxon","mouse_gene")
+cns_max_cell_types <- get_max_expression(cns_cell_types,"cns_cell_type_taxon","mouse_gene")
 #for those genes that had a max expression level of 0, updated the cell_type description
-cns_max_cell_types %<>% mutate(cell_type_taxon = if_else(expression_levels == 0,"Gene detected; No expression measured", cell_type_taxon))
-
-# cns_max_cell_types %>% write_csv(path = here("ProcessedData", "ZeiselEtAl", "max_cns_cell_type_expression.csv"))
+cns_max_cell_types %<>% mutate(cns_cell_type_taxon = if_else(expression_levels == 0,"Gene detected; No expression measured", cns_cell_type_taxon))
 
 
 ####################################################################
@@ -58,10 +57,63 @@ howard_mouse_genes %<>% mutate(mouseGene = if_else(gene_symbol_upper == "OR2B2",
 
 Howard_Table <- howard %>% mutate(gene_symbol_upper = toupper(gene_symbol))
 Howard_Table %<>% left_join(howard_mouse_genes %>% select(gene_symbol_upper, mouseGene), by = c('gene_symbol_upper' = 'gene_symbol_upper'))
-Howard_Table %<>% left_join(cns_max_cell_types %>% select(mouse_gene,cell_type_taxon), by = c('mouseGene' = 'mouse_gene'))
-Howard_Table %<>% rename(cns_cell_type_taxon = cell_type_taxon)
+Howard_Table %<>% left_join(cns_max_cell_types %>% select(mouse_gene,cns_cell_type_taxon), by = c('mouseGene' = 'mouse_gene'))
 Howard_Table %<>% left_join(max_cell_types %>% select(mouse_gene, cell_type_taxon), by = c('mouseGene' = 'mouse_gene'))
 Howard_Table %<>% distinct() %>% select(-gene_symbol_upper)
 #Check if changes were applies correctly
 Howard_Table %>% filter(gene_symbol == "OR2B2") #should map to one cell-type taxon
 Howard_Table %>% write_csv(here("Processed_Data/ZeiselEtAl/HowardCellTypes.csv"))
+
+
+# #############################calculate the probability for the cell types
+cell_pop <- nrow(max_cell_types)
+cell_count <- max_cell_types %>% group_by(cell_type_taxon) %>% summarize(genome_cell_count = n())
+
+howard_cell <- Howard_Table
+howard_cell %<>% mutate(cell_type_taxon = ifelse(cell_type_taxon == "character(0)", NA, cell_type_taxon))
+howard_count <- howard_cell %>% group_by(cell_type_taxon) %>% summarise(sample_cell_count = n())
+
+full_cell_count <- cell_count %>% left_join(howard_count, by=c('cell_type_taxon' = 'cell_type_taxon') )
+# perform hypergeometric test - read in file for hyper_test function
+source(here("R/transcriptomic_meta/hyper_test.R"))
+#subtract one for the gene that wasn't assigned a cell type but was observed
+cell_expected_probs  <- full_count %>% rowwise() %>% mutate(hypergeometric_p = hyper_test(sample_cell_count, genome_cell_count, cell_pop, colSums(na.omit(howard_count)[,2]) - 1))
+#corrected by the number of possible cell types to choose from subtract one because of the 'gene not expressed option'
+cell_expected_probs %<>% mutate(corrected_hypergeometric_p = p.adjust(hypergeometric_p, method = "bonferroni", n= (nrow(cell_count) - 1)))
+cell_expected_probs %<>% mutate(corrected_hypergeometric_p = ifelse(cell_type_taxon == "Gene detected; No expression measured", NA, corrected_hypergeometric_p))
+cell_expected_probs %<>% arrange(hypergeometric_p)
+
+
+# #################################
+cns_cell_pop <- nrow(cns_max_cell_types)
+cns_cell_count <- cns_max_cell_types %>% group_by(cns_cell_type_taxon) %>% summarize(genome_cell_count = n())
+
+cns_howard_cell <- Howard_Table
+cns_howard_cell %<>% mutate(cns_cell_type_taxon = ifelse(cns_cell_type_taxon == "character(0)", NA, cns_cell_type_taxon))
+cns_howard_count <- cns_howard_cell %>% group_by(cns_cell_type_taxon) %>% summarise(sample_cell_count = n())
+
+#full_count <- howard_count %>% left_join(cell_count, by=c('cell_types' = 'cell_types') )
+cns_full_count <- cns_cell_count %>% left_join(cns_howard_count, by=c('cns_cell_type_taxon' = 'cns_cell_type_taxon') )
+
+#subtract one for the gene that wasn't assigned a cell type but was observed
+cns_cell_expected_probs  <- cns_full_count %>% rowwise() %>% mutate(hypergeometric_p = hyper_test(sample_cell_count, genome_cell_count, cell_pop, colSums(na.omit(howard_count)[,2]) - 1))
+#corrected by the number of possible cell types to choose from subtract one because of the 'gene not expressed option'
+cns_cell_expected_probs %<>% mutate(corrected_hypergeometric_p = p.adjust(hypergeometric_p, method = "bonferroni", n = nrow(cell_count)-1))
+cns_cell_expected_probs %<>% mutate(corrected_hypergeometric_p = ifelse(cns_cell_type_taxon == "Gene detected; No expression measured", NA, corrected_hypergeometric_p))
+cns_cell_expected_probs %<>% arrange(hypergeometric_p)
+
+
+#upload to google drive
+sheets_auth(token = drive_token())
+
+cells <- drive_get("~/Thesis/Manuscript/Supplement_Tables/cell_hyper_expected")
+if(nrow(cells) != 0) {
+  drive_rm(cells)
+}
+#create the google worksheet
+cells <- sheets_create("cell_hyper_expected",sheets = c('hypergeometric_cell_type_taxons','hypergeometric_cns_cell_type_taxons'))
+sheets_write(cell_expected_probs, cells,  sheet = "hypergeometric_cell_type_taxons")
+sheets_write(cns_cell_expected_probs, cells,  sheet = "hypergeometric_cns_cell_type_taxons")
+
+drive_mv(file = "cell_hyper_expected", path = "~/Thesis/Manuscript/Supplement_Tables/")  # move Sheets file
+
