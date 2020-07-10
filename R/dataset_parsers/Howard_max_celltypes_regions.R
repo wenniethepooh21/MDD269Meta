9556562079fc6e45e9e153578f97ffdabe340ac2
Wennie <cutewennie@hotmail.com>
2020-07-10 09:32:02 -0400

    removed duplicated entry for DCDC1 gene

--- R/dataset_parsers/Howard_max_celltypes_regions.R
+++ R/dataset_parsers/Howard_max_celltypes_regions.R
@@ -4,7 +4,7 @@ library(magrittr)
 library(dplyr)
 
 #This script matches the howard 269 genes to the max brain region and cell type taxon that expressed it
-howard_regions <- read_csv(here("Processed_Data/HowardEtAl/HowardRegions_four.csv"))
+howard_regions <- read_csv(here("Processed_Data/HowardEtAl/HowardRegions_four.csv")) %>% distinct()
 howard_cell_types <- read_csv(here("Processed_Data/ZeiselEtAl/HowardCellTypes.csv"))
 #merge the two datasets together 
 howard_region_cell_types <- howard_regions %>% left_join(howard_cell_types)
