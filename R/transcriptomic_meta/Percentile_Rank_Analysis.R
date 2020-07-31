c8688466d04041ee9c8cdd7e9ab989466520ff16
Wennie <cutewennie@hotmail.com>
2020-07-31 08:44:13 -0400

    updated files with new percentile rank function

--- R/transcriptomic_meta/Percentile_Rank_Analysis.R
+++ R/transcriptomic_meta/Percentile_Rank_Analysis.R
@@ -8,34 +8,10 @@ getRank <- function(dataset){
   print("Running rank calculations.. ")
   #get the number of genes
   num_genes <- (dataset %>% select(gene_symbol) %>% nrow())
-  dataset %<>% mutate(meta_Up = NA, meta_Down = NA)
-  #filter for all genes that have higher expression levels
-  higher_expression <- dataset %>% filter(meta_direction == +1)
-  #filter all genes that have lower expression levels
-  lower_expression <- dataset %>% filter(meta_direction == -1) 
-  for (i in 1:num_genes) {
-    if(i == ceiling(num_genes/2)){
-      print("50% complete")
-    }
-    #get current genes calculated meta_p value
-    gene_meta <- dataset$meta_p[i]
-    #get current genes calculated meta direction
-    direction_val <- dataset$meta_direction[i]
-    #compare the current genes direction with the rest of the genome 
-    if(direction_val == +1) {      
-      #this current gene will be in the down regulation list, offset it
-      dataset$meta_Up[i] <- higher_expression %>% filter(meta_p <= gene_meta) %>% select(gene_symbol) %>% nrow() / num_genes
-      dataset$meta_Down[i] <- (lower_expression %>% filter(meta_p <= gene_meta) %>% select(gene_symbol) %>% nrow() + 1) / (num_genes + 1)
-    
-     }else if (direction_val == -1) {
-      dataset$meta_Up[i] <- (higher_expression %>% filter(meta_p <= gene_meta) %>% select(gene_symbol) %>% nrow() + 1) / (num_genes + 1)
-      dataset$meta_Down[i] <- lower_expression %>% filter(meta_p <= gene_meta) %>% select(gene_symbol) %>% nrow() / num_genes
-    }
-    
-  }
-  print("Complete!")
+  dataset %<>% mutate(meta_Up = rank(meta_higher_in_MDD_pvalue)/num_genes)
+  dataset %<>% mutate(meta_Down = rank(meta_lower_in_MDD_pvalue)/num_genes)
+  
   dataset %<>% rowwise() %>% mutate(empirical_meta_p = if_else(2 * min(meta_Up, meta_Down) > 1, 1, 2 * min(meta_Up, meta_Down))) %>% ungroup()
   return(dataset)
 }
-
 
